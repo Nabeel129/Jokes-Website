68456993ecf39c64a4d685e6fbd8d56471f76d15
Nabeel <nabeelahmed1129@gmail.com>
2022-09-21 16:06:05 +0500

    Random Jokes Generator

---
+++ jokes.js
@@ -0,0 +1,127 @@
+let jokes = [
+
+    `Mareez: Doctor Sahib Aap Ne Nurse Tou Buhat Achi Rakhi Hui Hai,
+
+Ussay Dekhte He Mein Theek Ho Gya.
+
+Doctor: Han Janta Hun! Thappar Ki Awaz Bahir Tk Aayi Thi 😂`,
+
+
+    `Pehla Wakeel: Mere Mokal Kiharkat Dekhi ?
+
+Doosra Wakeel: Kiun Kya Hua ?
+
+Pehla Wakeel: Yaar Mein Ne Ussay Jaali Noton Ke Muqaddamy Se Bari Karaya
+
+Or Woh Kambakhat Mujhe Fees Mein Wohi Note De Gya. 🤣`,
+
+
+
+    `Aik Pakistani Khatoon Ne Aik Cheeni Aadmi Se Shadi Ki. 5, 6 Mahinon Baad
+
+Uske Shohar Ka Intiqal Ho Gya.
+
+Taziat Ko Aayi Aik Khatoon Ne Kuch Youn Unko Hosla Diya:
+
+Himmmat Karo Behan, China Ke Cheezain
+
+5, 6 Mahine Se Ziada Nahi Chaltein  😁`,
+
+
+    `Aik Aadmi Ne FM Radio Pe Caal Ki.
+
+Aadmi: MUjhe Aik Wallet Mila Hai Jis Mein Buhat Saare Paise Hain,
+
+Address Baharabad Ka Hai__
+
+HOst: Tou Aap Inhain Wallet Wapis Dena Chahte Hain ?
+
+Aadmi : Pagal Samjha Hua Hai ? Meri Taraf Se Usse Sad Song Dedicate Kar Dain.`,
+
+
+
+    `Sunya Ki Sab Se Patli Kitaab Ka Naam Hai:
+
+“Auratain Kya Chahti Hain”
+
+Andar Aik Safeh Per Sirf Aik Lafaz Likha Hai
+
+“Har Cheez”  😆`,
+
+
+    `Bewakoof Shehri:
+
+Yeh Saamne Jo Gaye Nazar Aa Rahi Hai Ess Ke Seeng Kiun Nahi Hain ?
+
+Dehati : Seeng Na Hone Ki Nuhat Si Wajuhaat Hoti Hain, Baaz Ke Seeng
+
+Toot Jaate Hain, Or Baaz Ke Hum Kaat Dete Hain, Baaqi Rahi Woh Saamne Wali Gaye
+
+Tou Us Ke Seeng Ess Liye Nahi Hain Keh Wo Gaye Nahi, Ghoora Hai.  😂`,
+
+
+
+    `Maa: Munne! Yeh Darwaze Par Gande Hathon Ke Nishanat Tumhare Hain ?
+
+Beta : Ji Nahi Ammi Jaan, Mein Tou Laat Maar Kar Darwaza Kholta Hun. 😄`,
+
+
+    `Aik Shakhs: Mein Aaj Aik Bare Aadmi Ki Jaib Kaati.
+
+Doosra: Kamal Hai______ Akhir Kaise ?
+
+Pehla : Mein Darzi Hoon. 😜`,
+
+
+
+    `Biwi: Tum Ne Shadi Se Pehle Kiun Nahi Btaya Keh Tum Ghareeb Ho:
+
+Shohar: Hazar Baar Btaya Tha Keh Tumhare Elawa Mere Pass Kuch Nahi Hai,
+
+Tum Yeh Baat Sun Kar Paglon Ki Tarah Hansne Lagti Thi. 😁`,
+
+
+    `Biwi Shohar Sy: Darling Lagta Hai Mein Buhat Moti Ho Gayi Hun Warna Pehle
+
+Tum Mujhe Farij Tak Banhon Mein Utha Kar Lekr Jaate Thy Or Ice Cream Khilate Thy.
+
+Shohar Jhagre Ke Khouf Se Nahi Jan Aisi Tou Koi Baat Nahi.
+
+Biwi: Chalo Phir Aaj Pehle Ki Tarah Mujhe Utha Kar Farij Tak Lekr Jao Or Icecream Khilao.
+
+Shohar Pareshan Ho Kar: Darling! Tum Yaheen Betho Aaj Mein Farij Utha Kar Lata Hun
+
+😁😂🤣`,
+
+
+
+    `Saas: Janti Ho Bahoo: Jab Mein Umeed Se Thi, Meine Jo Kuch Khaya,
+
+Aaj Mere Bete Ko Har Woh Cheez Pasand Hai
+
+Bahoo: Lekin Aapko Kam Az Kam Charas Or Sharab Se Parhaiz Karna Chahiye Tha ______😝`,
+
+
+    `Biwi: Hamari Parosan Har Maheene Apne Shohar Ke Sath Ghoomne Jati Hai.
+
+Shohar: Tou Phir ?
+
+Biwi : Tou Aap Bhi Kabhi Le Jaya Karain Na!
+
+Shohar: Mein 3, 4 Bar Kaha Hai, Par Woh Maanti Hi Nahi______🤣`,
+
+
+
+    `Aik Biwi Ne Mote Mote Ansoobaha Kar Shohar Se Kaha:
+
+Is Lockdown Ki Wajah Se Meri Hasiat Aik Nokrani Jaisi Ho Gayi Hai.
+
+Shohar: Meine Kapre Dhoye, Jharoo Pocha Se Lekr Khana Pakana 
+
+Sab Kaam Tou Meine Kiye Tum Kaise Nokarani Ho Gai ?
+
+Biwi Ne Masomiat Se Jawab Diya:
+Dil Ka Operation Karte Huye Doctor Hansne Laga.
+
+Nurse; Aap Kiun Hans Rahe Hain ?
+Doctor Bola: Pehli Baar Dil Khol Kar Hansne Ka Mouqa Mila Hai.`]
